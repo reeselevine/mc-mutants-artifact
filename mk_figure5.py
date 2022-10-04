0e0ca4a442e2ee6de10ae140001343ce5aaf71a1
Reese Levine <reeselevine1@gmail.com>
2022-10-04 16:40:11 -0700

    rename

--- mk_figure5.py
+++ mk_figure5.py
@@ -7,6 +7,7 @@ AMD = "amd"
 NVIDIA = "nvidia"
 M1 = "m1"
 devices = [INTEL, AMD, NVIDIA, M1]
+figure_labels = ["Intel", "AMD", "NVIDIA", "M1"]
 
 SITE_BASELINE = "site_baseline"
 SITE_BASELINE_COLOR = "#66c2a5"
@@ -111,19 +112,16 @@ def take_log(data, base=10):
             ret.append(math.log(v, base))
     return ret
 
-def take_pct(data, total):
-    return [v/total * 100 for v in data]
+def log(data, base=10):
+    if data < 10:
+        return data
+    else:
+        return math.log(data, base)
 
 def pct(data, total):
     return data/total * 100
 
 def mutation_score_subfigure(stats, mutant_type, num_tests, fig_name):
-    figure_labels = ["Intel", "AMD", "NVIDIA", "M1"]
-    pct_labels = ["0%", "20%", "40%", "60%", "80%", "100%"]
-    x = np.arange(len(figure_labels))
-    y = np.arange(0, 110, 20)
-    width = 0.2
-
     site_baseline_score = []
     site_score = []
     pte_baseline_score = []
@@ -134,6 +132,10 @@ def mutation_score_subfigure(stats, mutant_type, num_tests, fig_name):
         pte_baseline_score.append(pct(stats[PTE_BASELINE][device][mutant_type][CAUGHT], num_tests))
         pte_score.append(pct(stats[PTE][device][mutant_type][CAUGHT], num_tests))
 
+    pct_labels = ["0%", "20%", "40%", "60%", "80%", "100%"]
+    x = np.arange(len(figure_labels))
+    y = np.arange(0, 110, 20)
+    width = 0.2
     fig, ax = plt.subplots(1, 1, figsize=(5, 2))
     ax.bar(x - 1.5 * width, site_baseline_score, width, label="SITE Baseline", color=SITE_BASELINE_COLOR)
     ax.bar(x - .5 * width, site_score, width, label="SITE", color=SITE_COLOR)
@@ -145,15 +147,43 @@ def mutation_score_subfigure(stats, mutant_type, num_tests, fig_name):
     ax.set_ylabel("Mutation Score", fontsize=10)
     fig.legend(loc=(0.01, 0.87), fontsize=10, ncol=4)
     plt.tight_layout(rect=[0, 0, 1, .93])
-    plt.savefig("{}.pdf".format(fig_name))
+    plt.savefig("figure5{}.pdf".format(fig_name))
 
+def avg_mutant_death_rate_subfigure(stats, mutant_type, fig_name):
+    site_baseline_rates = []
+    site_rates = []
+    pte_baseline_rates = []
+    pte_rates = []
+    for device in devices:
+        site_baseline_rates.append(log(stats[SITE_BASELINE][device][mutant_type][AVG_RATE]))
+        site_rates.append(log(stats[SITE][device][mutant_type][AVG_RATE]))
+        pte_baseline_rates.append(log(stats[PTE_BASELINE][device][mutant_type][AVG_RATE]))
+        pte_rates.append(log(stats[PTE][device][mutant_type][AVG_RATE]))
 
-def make_figure(stats):
-    mutation_score_subfigure(stats, REVERSING_PO, REVERSING_PO_TESTS, "figure5a")
+    log_labels = ["0", "1", "10", "100", "1,000", "10,000", "100,000"]
+    x = np.arange(len(figure_labels))
+    y = [0, 1, 10, 100, 1000, 10000, 100000]
+    fig, ax = plt.subplots(1, 1, figsize=(5, 2))
+    ax.bar(x - 1.5 * width, site_baseline_rates, width, color=SITE_BASELINE_COLOR)
+    ax.bar(x - .5 * width, site_rates, width, color=SITE_COLOR)
+    ax.bar(x + .5 * width, pte_baseline_rates, width, color=PTE_BASELINE_COLOR)
+    ax.bar(x + 1.5 * width, pte_rates, width, color=PTE_COLOR)
+    ax.set_yscale('symlog')
+    ax.set_yticks(y, log_labels, fontsize=10)
+    ax.set_xticks(x, labels)
+    ax.set_ylabel("Mutant Death Rate (sec)", fontsize=10)
+    plt.tight_layout(rect=[0, 0, 1, .93])
+    plt.savefig("figure5{}.pdf".format(fig_name))
+
+def make_figures(stats):
+    mutation_score_subfigure(stats, REVERSING_PO, REVERSING_PO_TESTS, "a")
+    avg_mutant_death_rate_subfigure(stats, REVERSING_PO, "b")
+    mutation_score_subfigure(stats, WEAKENING_PO, WEAKENING_PO_TESTS, "c")
+    mutation_score_subfigure(stats, WEAKENING_SW, WEAKENING_SW_TESTS, "e")
 
 def main():
     stats = all_stats()
-    make_figure(stats)
+    make_figures(stats)
 
 if __name__ == "__main__":
     main()
