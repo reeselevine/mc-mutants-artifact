fe6b8405d2ca852e76a865d751408965009d313b
Reese Levine <reeselevine1@gmail.com>
2022-10-04 17:06:31 -0700

    Add data files and finish figure 5

--- mk_figure5.py
+++ mk_figure5.py
@@ -1,4 +1,5 @@
 import json
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -26,7 +27,7 @@ WEAKENING_PO_TESTS = 6
 WEAKENING_SW = "weakening_sw"
 WEAKENING_SW_TESTS = 18
 ALL = "all"
-TOTAL_TESTS = 32
+ALL_TESTS = 32
 mutant_categories = [REVERSING_PO, WEAKENING_PO, WEAKENING_SW, ALL]
 CAUGHT = "caught"
 AVG_RATE = "avg_rate"
@@ -103,37 +104,33 @@ def all_stats():
         result[e] = per_device_stats(e)
     return result
 
-def take_log(data, base=10):
-    ret = []
-    for v in data:
-        if v < 10:
-            ret.append(v)
-        else:
-            ret.append(math.log(v, base))
-    return ret
-
-def log(data, base=10):
-    if data < 10:
-        return data
-    else:
-        return math.log(data, base)
-
 def pct(data, total):
     return data/total * 100
 
-def mutation_score_subfigure(stats, mutant_type, num_tests, fig_name):
+def mutation_score_subfigure(stats, mutant_type, num_tests, fig_name, per_device=True):
     site_baseline_score = []
     site_score = []
     pte_baseline_score = []
     pte_score = []
-    for device in devices:
-        site_baseline_score.append(pct(stats[SITE_BASELINE][device][mutant_type][CAUGHT], num_tests))
-        site_score.append(pct(stats[SITE][device][mutant_type][CAUGHT], num_tests))
-        pte_baseline_score.append(pct(stats[PTE_BASELINE][device][mutant_type][CAUGHT], num_tests))
-        pte_score.append(pct(stats[PTE][device][mutant_type][CAUGHT], num_tests))
+    if per_device:
+        for device in devices:
+            site_baseline_score.append(pct(stats[SITE_BASELINE][device][mutant_type][CAUGHT], num_tests))
+            site_score.append(pct(stats[SITE][device][mutant_type][CAUGHT], num_tests))
+            pte_baseline_score.append(pct(stats[PTE_BASELINE][device][mutant_type][CAUGHT], num_tests))
+            pte_score.append(pct(stats[PTE][device][mutant_type][CAUGHT], num_tests))
+    else:
+        site_baseline_score.append(pct(stats[SITE_BASELINE][ALL][CAUGHT], num_tests))
+        site_score.append(pct(stats[SITE][ALL][CAUGHT], num_tests))
+        pte_baseline_score.append(pct(stats[PTE_BASELINE][ALL][CAUGHT], num_tests))
+        pte_score.append(pct(stats[PTE][ALL][CAUGHT], num_tests))
 
     pct_labels = ["0%", "20%", "40%", "60%", "80%", "100%"]
-    x = np.arange(len(figure_labels))
+    if per_device:
+        x = np.arange(len(figure_labels))
+        labels = figure_labels
+    else:
+        x = np.arange(1)
+        labels = ["Total"]
     y = np.arange(0, 110, 20)
     width = 0.2
     fig, ax = plt.subplots(1, 1, figsize=(5, 2))
@@ -143,35 +140,48 @@ def mutation_score_subfigure(stats, mutant_type, num_tests, fig_name):
     ax.bar(x + 1.5 * width, pte_score, width, label="PTE", color=PTE_COLOR)
     ax.set_ylim([0, 100])
     ax.set_yticks(y, pct_labels, fontsize=10)
-    ax.set_xticks(x, figure_labels)
+    ax.set_xticks(x, labels)
     ax.set_ylabel("Mutation Score", fontsize=10)
     fig.legend(loc=(0.01, 0.87), fontsize=10, ncol=4)
     plt.tight_layout(rect=[0, 0, 1, .93])
     plt.savefig("figure5{}.pdf".format(fig_name))
 
-def avg_mutant_death_rate_subfigure(stats, mutant_type, fig_name):
+def avg_mutant_death_rate_subfigure(stats, mutant_type, fig_name, per_device=True):
     site_baseline_rates = []
     site_rates = []
     pte_baseline_rates = []
     pte_rates = []
-    for device in devices:
-        site_baseline_rates.append(log(stats[SITE_BASELINE][device][mutant_type][AVG_RATE]))
-        site_rates.append(log(stats[SITE][device][mutant_type][AVG_RATE]))
-        pte_baseline_rates.append(log(stats[PTE_BASELINE][device][mutant_type][AVG_RATE]))
-        pte_rates.append(log(stats[PTE][device][mutant_type][AVG_RATE]))
+    if per_device:
+        for device in devices:
+            site_baseline_rates.append(stats[SITE_BASELINE][device][mutant_type][AVG_RATE])
+            site_rates.append(stats[SITE][device][mutant_type][AVG_RATE])
+            pte_baseline_rates.append(stats[PTE_BASELINE][device][mutant_type][AVG_RATE])
+            pte_rates.append(stats[PTE][device][mutant_type][AVG_RATE])
+    else:
+        site_baseline_rates.append(stats[SITE_BASELINE][ALL][AVG_RATE])
+        site_rates.append(stats[SITE][ALL][AVG_RATE])
+        pte_baseline_rates.append(stats[PTE_BASELINE][ALL][AVG_RATE])
+        pte_rates.append(stats[PTE][ALL][AVG_RATE])
 
     log_labels = ["0", "1", "10", "100", "1,000", "10,000", "100,000"]
-    x = np.arange(len(figure_labels))
+    if per_device:
+        x = np.arange(len(figure_labels))
+        labels = figure_labels
+    else:
+        x = np.arange(1)
+        labels = ["Total"]
     y = [0, 1, 10, 100, 1000, 10000, 100000]
+    width = 0.2
     fig, ax = plt.subplots(1, 1, figsize=(5, 2))
-    ax.bar(x - 1.5 * width, site_baseline_rates, width, color=SITE_BASELINE_COLOR)
-    ax.bar(x - .5 * width, site_rates, width, color=SITE_COLOR)
-    ax.bar(x + .5 * width, pte_baseline_rates, width, color=PTE_BASELINE_COLOR)
-    ax.bar(x + 1.5 * width, pte_rates, width, color=PTE_COLOR)
+    ax.bar(x - 1.5 * width, site_baseline_rates, width, label="SITE Baseline", color=SITE_BASELINE_COLOR)
+    ax.bar(x - .5 * width, site_rates, width, label="SITE", color=SITE_COLOR)
+    ax.bar(x + .5 * width, pte_baseline_rates, width, label="PTE Baseline", color=PTE_BASELINE_COLOR)
+    ax.bar(x + 1.5 * width, pte_rates, width, label="PTE", color=PTE_COLOR)
     ax.set_yscale('symlog')
     ax.set_yticks(y, log_labels, fontsize=10)
     ax.set_xticks(x, labels)
     ax.set_ylabel("Mutant Death Rate (sec)", fontsize=10)
+    fig.legend(loc=(0.01, 0.87), fontsize=10, ncol=4)
     plt.tight_layout(rect=[0, 0, 1, .93])
     plt.savefig("figure5{}.pdf".format(fig_name))
 
@@ -179,7 +189,13 @@ def make_figures(stats):
     mutation_score_subfigure(stats, REVERSING_PO, REVERSING_PO_TESTS, "a")
     avg_mutant_death_rate_subfigure(stats, REVERSING_PO, "b")
     mutation_score_subfigure(stats, WEAKENING_PO, WEAKENING_PO_TESTS, "c")
+    avg_mutant_death_rate_subfigure(stats, WEAKENING_PO, "d")
     mutation_score_subfigure(stats, WEAKENING_SW, WEAKENING_SW_TESTS, "e")
+    avg_mutant_death_rate_subfigure(stats, WEAKENING_SW, "f")
+    mutation_score_subfigure(stats, ALL, ALL_TESTS, "g")
+    avg_mutant_death_rate_subfigure(stats, ALL, "h")
+    mutation_score_subfigure(stats, "N/A", ALL_TESTS * len(devices), "i", False)
+    avg_mutant_death_rate_subfigure(stats, "N/A", "j", False)
 
 def main():
     stats = all_stats()
